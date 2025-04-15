281a0aae56716cc850d61436eb74f6e896db6ca3
Rencelagsil <lagsilrence@gmail.com>
2025-04-16 00:59:25 +0800

    .

--- student_registration.sql
+++ student_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2025 at 07:55 PM
+-- Generation Time: Apr 15, 2025 at 06:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -28,17 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account_balance` (
-  `student_id` int(11) NOT NULL,
-  `balance` decimal(10,2) DEFAULT 0.00
+  `id` int(11) NOT NULL,
+  `student_id` bigint(50) NOT NULL,
+  `balance` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `account_balance`
 --
 
-INSERT INTO `account_balance` (`student_id`, `balance`) VALUES
-(123456, 100.00),
-(2147483647, 1000.00);
+INSERT INTO `account_balance` (`id`, `student_id`, `balance`) VALUES
+(1, 2311600102, '200.00'),
+(2, 2311600103, '1000.00'),
+(3, 2311600104, '1200.00'),
+(4, 2311600101, '19000.00'),
+(5, 2311600100, '2000.00'),
+(6, 123412131313, '100.00');
 
 -- --------------------------------------------------------
 
@@ -48,7 +53,7 @@ INSERT INTO `account_balance` (`student_id`, `balance`) VALUES
 
 CREATE TABLE `grades` (
   `id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
+  `student_id` bigint(20) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `grade` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -58,7 +63,14 @@ CREATE TABLE `grades` (
 --
 
 INSERT INTO `grades` (`id`, `student_id`, `subject`, `grade`) VALUES
-(1, 123456, 'dada', '96');
+(8, 2311600102123, 'Math', '97'),
+(19, 2311600103, 'Arts', '90'),
+(20, 2311600103, 'Religon', '81'),
+(21, 2311600101, 'Math', '96'),
+(22, 2311600101, 'Arts', '81'),
+(23, 2311600101, 'PE', '86'),
+(24, 123456, 'Math', '75'),
+(25, 123412131313, 'Math', '89');
 
 -- --------------------------------------------------------
 
@@ -92,18 +104,22 @@ INSERT INTO `login` (`id`, `student_id`, `password`) VALUES
 --
 
 CREATE TABLE `permits` (
-  `id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `permit_type` varchar(200) NOT NULL,
-  `status` varchar(50) NOT NULL
+  `permit_id` int(11) NOT NULL,
+  `student_id` varchar(50) DEFAULT NULL,
+  `permit_type` varchar(50) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `issue_date` date DEFAULT NULL,
+  `expiration_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `permits`
 --
 
-INSERT INTO `permits` (`id`, `student_id`, `permit_type`, `status`) VALUES
-(1, 2147483647, 'da', 'Approved');
+INSERT INTO `permits` (`permit_id`, `student_id`, `permit_type`, `status`, `issue_date`, `expiration_date`) VALUES
+(1, '2311600101', 'Prelim', 'Pending', '2024-03-12', '2025-03-01'),
+(2, '2311600102', 'Midterm', 'Approved', '2025-01-04', '2025-03-05'),
+(3, '123412131313', 'Prelim', 'Approved', '2024-03-12', '2025-03-01');
 
 -- --------------------------------------------------------
 
@@ -116,19 +132,19 @@ CREATE TABLE `schedules` (
   `student_id` varchar(50) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `day` varchar(20) NOT NULL,
-  `time` varchar(20) NOT NULL
+  `time` varchar(20) NOT NULL,
+  `room` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `schedules`
 --
 
-INSERT INTO `schedules` (`id`, `student_id`, `subject`, `day`, `time`) VALUES
-(1, '2311600102', '', 'Monday', '9pm to 10pm'),
-(2, '2311600101', '', 'Monday', '9pm to 10pm'),
-(3, '2311600100', '', 'Monday', '9pm to 10pm'),
-(4, '2311600103', '', 'Monday', '9pm to 10pm'),
-(5, '2311600103', 'Math', 'Monday', '9pm to 10pm');
+INSERT INTO `schedules` (`id`, `student_id`, `subject`, `day`, `time`, `room`) VALUES
+(16, '123412131313', 'Arts', 'Friday', '8am to 9am', 'fgm101'),
+(17, '123412131313', 'Math', 'Tuesday', '11am to 12am', 'fgm103'),
+(18, '123412131313', 'PE', 'Monday', '11am to 12am', 'fgm105'),
+(19, '123412131313', 'Science', 'Friday', '8am to 9am', 'fgm101');
 
 -- --------------------------------------------------------
 
@@ -138,37 +154,28 @@ INSERT INTO `schedules` (`id`, `student_id`, `subject`, `day`, `time`) VALUES
 
 CREATE TABLE `students` (
   `id` int(11) NOT NULL,
-  `studentType` varchar(50) DEFAULT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `middle_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `ext_Name` varchar(10) DEFAULT NULL,
-  `birthdate` date DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `place` varchar(255) DEFAULT NULL,
-  `student_id` varchar(12) DEFAULT NULL,
-  `religion` varchar(50) DEFAULT NULL,
-  `gender` varchar(50) DEFAULT NULL,
-  `street_address` varchar(100) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `state_province` varchar(100) DEFAULT NULL,
-  `country` varchar(100) DEFAULT NULL,
-  `zip_code` varchar(10) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `contact_number` varchar(20) DEFAULT NULL,
-  `strand` varchar(100) DEFAULT NULL,
-  `semester` varchar(50) DEFAULT NULL,
-  `level` varchar(100) DEFAULT NULL
+  `fName` varchar(50) NOT NULL,
+  `mName` varchar(50) DEFAULT NULL,
+  `lName` varchar(50) NOT NULL,
+  `extName` varchar(10) DEFAULT NULL,
+  `birthdate` date NOT NULL,
+  `age` int(11) NOT NULL,
+  `place` varchar(100) NOT NULL,
+  `student_id` varchar(15) NOT NULL,
+  `religion` varchar(50) NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `zip` varchar(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contactNumber` varchar(15) NOT NULL,
+  `strand` varchar(10) NOT NULL,
+  `level` varchar(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` enum('user','admin') NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`id`, `studentType`, `first_name`, `middle_name`, `last_name`, `ext_Name`, `birthdate`, `age`, `place`, `student_id`, `religion`, `gender`, `street_address`, `city`, `state_province`, `country`, `zip_code`, `email`, `contact_number`, `strand`, `semester`, `level`) VALUES
-(1, 'New Student', 'Justine', '', 'Moril', '', '2003-08-12', 21, 'pol', '231160010322', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'fauzi@gmail.com', '09071787238', 'TVL', '2nd Semester', 'Grade 12'),
-(3, 'Old Student', 'Rence', 'Diamoda', 'Lagsil', '', '2004-08-12', 20, 'pol', '231160010322', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'lagsilrence@gmail.com', '09071787238', 'HUMSS', '2nd Semester', 'Grade 11'),
-(6, 'Transferee', 'Raph', '', 'Visayas', '', '2004-07-04', 20, 'pol', '123412131313', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'lagsilrence@gmail.com', '09071787238', 'TVL', '2nd Semester', 'Grade 12');
 
 --
 -- Indexes for dumped tables
@@ -178,7 +185,7 @@ INSERT INTO `students` (`id`, `studentType`, `first_name`, `middle_name`, `last_
 -- Indexes for table `account_balance`
 --
 ALTER TABLE `account_balance`
-  ADD PRIMARY KEY (`student_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `grades`
@@ -196,7 +203,7 @@ ALTER TABLE `login`
 -- Indexes for table `permits`
 --
 ALTER TABLE `permits`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`permit_id`);
 
 --
 -- Indexes for table `schedules`
@@ -218,13 +225,13 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `account_balance`
 --
 ALTER TABLE `account_balance`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `login`
@@ -236,19 +243,19 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `permits`
 --
 ALTER TABLE `permits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `permit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
