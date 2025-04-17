4c3bd3a71bb24ae69c78a68e54301323256f8669
Rencelagsil <lagsilrence@gmail.com>
2025-04-17 22:33:36 +0800

    update for sql

--- student_registration.sql
+++ student_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2025 at 06:56 PM
+-- Generation Time: Apr 17, 2025 at 04:32 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -43,7 +43,7 @@ INSERT INTO `account_balance` (`id`, `student_id`, `balance`) VALUES
 (3, 2311600104, '1200.00'),
 (4, 2311600101, '19000.00'),
 (5, 2311600100, '2000.00'),
-(6, 123412131313, '100.00');
+(6, 123412131313, '1100.00');
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,8 @@ INSERT INTO `grades` (`id`, `student_id`, `subject`, `grade`) VALUES
 (22, 2311600101, 'Arts', '81'),
 (23, 2311600101, 'PE', '86'),
 (24, 123456, 'Math', '75'),
-(25, 123412131313, 'Math', '89');
+(25, 123412131313, 'Math', '89'),
+(26, 231160010212, 'Math', '89');
 
 -- --------------------------------------------------------
 
@@ -95,7 +96,9 @@ INSERT INTO `login` (`id`, `student_id`, `password`) VALUES
 (4, '2311600103', 'Lathougs'),
 (5, '231160010322', 'Lathougs'),
 (6, '231160010325', 'Lathougs'),
-(7, '123412131313', 'Lathougs');
+(7, '123412131313', 'Lathougs'),
+(8, '231160010212', 'Lathougs'),
+(9, '231160010213', 'Lathougs');
 
 -- --------------------------------------------------------
 
@@ -154,28 +157,38 @@ INSERT INTO `schedules` (`id`, `student_id`, `subject`, `day`, `time`, `room`) V
 
 CREATE TABLE `students` (
   `id` int(11) NOT NULL,
-  `fName` varchar(50) NOT NULL,
+  `fName` varchar(50) DEFAULT NULL,
   `mName` varchar(50) DEFAULT NULL,
-  `lName` varchar(50) NOT NULL,
+  `lName` varchar(50) DEFAULT NULL,
   `extName` varchar(10) DEFAULT NULL,
-  `birthdate` date NOT NULL,
-  `age` int(11) NOT NULL,
-  `place` varchar(100) NOT NULL,
-  `student_id` varchar(15) NOT NULL,
-  `religion` varchar(50) NOT NULL,
-  `gender` enum('Male','Female') NOT NULL,
-  `street` varchar(100) NOT NULL,
-  `city` varchar(50) NOT NULL,
-  `state` varchar(50) NOT NULL,
-  `country` varchar(50) NOT NULL,
-  `zip` varchar(10) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `contactNumber` varchar(15) NOT NULL,
-  `strand` varchar(10) NOT NULL,
-  `level` varchar(10) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `role` enum('user','admin') NOT NULL DEFAULT 'user'
+  `birthdate` date DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `place` varchar(100) DEFAULT NULL,
+  `student_id` varchar(20) DEFAULT NULL,
+  `religion` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `street` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `contactNumber` varchar(20) DEFAULT NULL,
+  `strand` varchar(50) DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL,
+  `semester` varchar(20) DEFAULT NULL,
+  `school_year` varchar(20) DEFAULT NULL,
+  `role` varchar(20) DEFAULT 'user',
+  `registered_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `fName`, `mName`, `lName`, `extName`, `birthdate`, `age`, `place`, `student_id`, `religion`, `gender`, `street`, `city`, `state`, `country`, `zip`, `email`, `contactNumber`, `strand`, `level`, `semester`, `school_year`, `role`, `registered_at`) VALUES
+(1, 'Rence', 'Diamoda', 'Lagsil', '', '2003-08-12', 18, 'pol', '123412131313', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'lagsilrence@gmail.com', '09071787238', 'HUMSS', 'Grade 11', '2nd Semester', '2026-2027', 'user', '2025-04-17 08:00:33'),
+(22, 'Justine', 'laso', 'moril', '', '2004-08-12', 0, 'Polomolok', '231160010213', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'fauzi@gmail.com', '09071787239', 'TVL', 'Grade 11', '1st Semester', NULL, 'user', '2025-04-17 11:50:03');
 
 --
 -- Indexes for dumped tables
@@ -215,7 +228,10 @@ ALTER TABLE `schedules`
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id` (`student_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `contactNumber` (`contactNumber`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -231,13 +247,13 @@ ALTER TABLE `account_balance`
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `permits`
@@ -255,7 +271,7 @@ ALTER TABLE `schedules`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
