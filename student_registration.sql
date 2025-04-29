c94337751575e813b4e6f8434b74bd32fdb2d5a0
Rencelagsil <lagsilrence@gmail.com>
2025-04-30 00:58:37 +0800

    sql

--- student_registration.sql
+++ student_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2025 at 04:22 PM
+-- Generation Time: Apr 29, 2025 at 06:57 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -74,7 +74,8 @@ INSERT INTO `grades` (`id`, `student_id`, `subject`, `grade`) VALUES
 (26, 231160010212, 'Math', '89'),
 (27, 123412131313, 'Math', '90'),
 (28, 231160010213, 'Arts', '89'),
-(29, 231160010213, 'Religon', '75');
+(29, 231160010213, 'Religon', '75'),
+(30, 231160010214, 'Science', '92');
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,6 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `student_id`, `password`) VALUES
-(1, '2311600102', 'Lathougs'),
 (2, '2311600104', 'Lathougs'),
 (3, '2311600101', 'Lathougs'),
 (4, '2311600103', 'Lathougs'),
@@ -102,7 +102,11 @@ INSERT INTO `login` (`id`, `student_id`, `password`) VALUES
 (7, '123412131313', 'Lathougs'),
 (8, '231160010212', 'Lathougs'),
 (9, '231160010213', 'Lathougs'),
-(10, '231160010211', 'Lathougs');
+(10, '231160010211', 'Lathougs'),
+(11, '231160010215', 'Lathougs'),
+(12, '231160010214', 'Lathougs'),
+(13, '231160010104', 'Lathougs'),
+(14, '231160010210', 'Lathougs');
 
 -- --------------------------------------------------------
 
@@ -183,6 +187,7 @@ CREATE TABLE `students` (
   `level` varchar(20) DEFAULT NULL,
   `semester` varchar(20) DEFAULT NULL,
   `school_year` varchar(20) DEFAULT NULL,
+  `is_approved` enum('approved','reject') NOT NULL,
   `registered_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -190,15 +195,15 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `student_type`, `fName`, `mName`, `lName`, `extName`, `birthdate`, `age`, `place`, `student_id`, `religion`, `gender`, `street`, `city`, `state`, `country`, `zip`, `email`, `contactNumber`, `strand`, `level`, `semester`, `school_year`, `registered_at`) VALUES
-(1, 'New Student', 'Justine', '', 'Moril', '', '2003-08-12', 12, 'Polomolok', '231160010213', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'ffauzi@gmail.com', '09071787237', 'ABM', 'Grade 11', '1st Semester', '2025-2026', '2025-04-23 11:16:47'),
-(2, 'New Student', 'Justine', 'Diamoda', 'moril', '', '2003-08-12', 12, 'Polomolok', '231160010213', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'ffauzi@gmail.com', '09071787237', 'ABM', 'Grade 11', '1st Semester', '2025-2026', '2025-04-23 11:17:00'),
-(3, 'Old Student', 'Justine', '', 'moril', '', '0000-00-00', 0, '', '231160010212', '', '', '', '', '', '', '', 'ffauzi@gmail.com', '09071787237', 'STEM', 'Grade 11', '1st Semester', '', '2025-04-23 11:22:12'),
-(4, 'Old Student', 'Johar', '', 'Gogo', '', '2003-08-12', 20, 'Polomolok', '231160010211', 'ambot', 'Male', 'Brgy. Poblacion', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'ffauzi@gmail.com', '09071787237', 'GAS', 'Grade 12', '1st Semester', '2025-2026', '2025-04-23 11:27:33'),
-(5, 'Old Student', 'hehe', '', 'Gogor', '', '0000-00-00', 0, '', '231160010210', '', '', '', '', '', '', '', 'ffauzi@gmail.com', '09071787237', 'STEM', 'Grade 11', '1st Semester', '', '2025-04-23 11:31:21'),
-(6, 'Old Student', 'hehe', '', 'Gogor', '', '2003-08-12', 0, '', '231160010210', '', '', '', '', '', '', '', 'ffauzi@gmail.com', '09071787237', 'HUMSS', 'Grade 11', '1st Semester', '', '2025-04-23 11:34:16'),
-(7, 'Transferee Student', 'Raph', 'Sho', 'Visayas', '', '2003-08-12', 21, 'Polomolok', '231160010213', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'fauzi@gmail.com', '09071787237', 'HUMSS', 'Grade 11', '2nd Semester', '2025-2026', '2025-04-23 11:35:58'),
-(8, 'Old Student', 'Raph', '', 'Visayas', '', '0000-00-00', 21, '', '231160010201', '', '', '', '', '', '', '', 'fauzi@gmail.com', '09071787237', 'STEM', 'Grade 12', '1st Semester', '', '2025-04-23 11:40:48');
+INSERT INTO `students` (`id`, `student_type`, `fName`, `mName`, `lName`, `extName`, `birthdate`, `age`, `place`, `student_id`, `religion`, `gender`, `street`, `city`, `state`, `country`, `zip`, `email`, `contactNumber`, `strand`, `level`, `semester`, `school_year`, `is_approved`, `registered_at`) VALUES
+(3, 'Old Student', 'Justine', '', 'moril', '', '0000-00-00', 0, '', '231160010212', '', '', '', '', '', '', '', 'ffauzi@gmail.com', '09071787237', 'STEM', 'Grade 11', '1st Semester', '', 'approved', '2025-04-23 11:22:12'),
+(4, 'Old Student', 'Johar', '', 'Gogo', '', '2003-08-12', 20, 'Polomolok', '231160010211', 'ambot', 'Male', 'Brgy. Poblacion', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'ffauzi@gmail.com', '09071787237', 'GAS', 'Grade 12', '1st Semester', '2025-2026', 'approved', '2025-04-23 11:27:33'),
+(6, 'Old Student', 'hehe', '', 'Gogor', '', '2003-08-12', 0, '', '231160010210', '', '', '', '', '', '', '', 'ffauzi@gmail.com', '09071787237', 'HUMSS', 'Grade 11', '1st Semester', '', 'approved', '2025-04-23 11:34:16'),
+(7, 'Transferee Student', 'Raph', '', 'Visayas', '', '2003-08-12', 21, 'Polomolok', '231160010213', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'fauzi@gmail.com', '09071787237', 'HUMSS', 'Grade 11', '2nd Semester', '2025-2026', 'approved', '2025-04-23 11:35:58'),
+(8, 'Old Student', 'Raph', '', 'Visayas', '', '0000-00-00', 21, '', '231160010201', '', '', '', '', '', '', '', 'fauzi@gmail.com', '09071787237', 'STEM', 'Grade 12', '1st Semester', '', 'approved', '2025-04-23 11:40:48'),
+(10, 'Old Student', 'Justine', '', 'Moril', '', '2004-08-12', 20, '', '231160010215', '', '', '', '', '', '', '', 'ffauzi@gmail.com', '09071787235', 'GAS', 'Grade 11', '1st Semester', '', 'approved', '2025-04-23 14:41:22'),
+(11, 'New Student', 'Johar', '', 'Gogo', '', '2005-08-12', 19, 'Polomolok', '231160010218', 'ambot', 'Male', 'Polomolok, South Cotabato', 'Polomolok', 'South Cotabato', 'Philippines', '9504', 'ffauzi@gmail.com', '09071787231', 'GAS', 'Grade 11', '2nd Semester', '2026-2027', 'approved', '2025-04-23 14:43:39'),
+(18, 'Old Student', 'Benjie', '', 'Glenogo', '', '2003-08-12', 20, '', '231160010104', '', '', '', '', '', '', '', 'benjie@gmail.com', '09359544536', 'HUMSS', 'Grade 12', '1st Semester', '', 'approved', '2025-04-29 16:54:22');
 
 --
 -- Indexes for dumped tables
@@ -254,13 +259,13 @@ ALTER TABLE `account_balance`
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `permits`
@@ -278,7 +283,7 @@ ALTER TABLE `schedules`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
